3ff642b5414b0b6bdb4890cb61c3f0e343c28cf8
zafchiel <bialakropla127.9@o2.pl>
2023-07-01 17:31:04 +0200

    working example / inefficient - will change

--- src/app/api/newAlias/route.ts
+++ src/app/api/newAlias/route.ts
@@ -3,24 +3,26 @@ import { kv } from "@vercel/kv"
 import { generateRandomAlias } from "@/lib/generateRandomAlias"
 
 export async function POST(request: Request) {
-  const body = await request.json()
+  const { url } = await request.json()
   const alias = generateRandomAlias()
 
   const res = await kv.get(alias)
 
   if (res === null) {
+    console.log("Creating new alias")
     try {
       await kv.set(alias, {
-        url: body.url,
+        url: url,
         alias,
         createdAt: new Date().toISOString(),
       })
-      const al = await kv.get(alias)
-      return NextResponse.json(al)
+      const newAlias = await kv.get(alias)
+      return NextResponse.json(newAlias)
     } catch (error) {
       console.log(error)
-      return NextResponse.json({ error })
+      return NextResponse.json(error)
     }
   }
-  return NextResponse.json({ res })
+  console.log("Alias already exists")
+  return NextResponse.json(res)
 }
--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -3,39 +3,59 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { kv } from "@vercel/kv"
 import { ChangeEvent, FormEvent, useEffect, useState } from "react"
-import { Url } from "url"
+import Link from "next/link"
 
 export default function FormComponent() {
   const [inputUrl, setInputUrl] = useState<string>("")
+  const [shortenedUrl, setshortenedUrl] = useState<string>("")
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
-    const res = await kv.set("url", inputUrl)
-    console.log(res)
+    try {
+      const res = await fetch("/api/newAlias", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ url: inputUrl }),
+      })
+      const data = await res.json()
+      setshortenedUrl(window.location.origin + "/" + data.alias)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   useEffect(() => {}, [])
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Label htmlFor="url">Paste your link here</Label>
-      <div className="flex">
-        <Input
-          type="url"
-          id="url"
-          value={inputUrl}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setInputUrl(e.target.value)
-          }
-          placeholder="https://example.com"
-          className="rounded-r-none"
-        />
-        <Button type="submit" className="rounded-l-none">
-          Create
-        </Button>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Label htmlFor="url">Paste your link here</Label>
+        <div className="flex">
+          <Input
+            type="url"
+            id="url"
+            value={inputUrl}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setInputUrl(e.target.value)
+            }
+            placeholder="https://example.com"
+            className="rounded-r-none"
+          />
+          <Button type="submit" className="rounded-l-none">
+            Create
+          </Button>
+        </div>
+      </form>
+      <div className="flex items-center justify-center p-3">
+        <Link href={shortenedUrl} passHref>
+          <h3 className="cursor-pointer text-xl font-bold underline">
+            {shortenedUrl}
+          </h3>
+        </Link>
       </div>
-    </form>
+    </>
   )
 }
